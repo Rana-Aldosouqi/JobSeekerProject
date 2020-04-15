ee4ce11d4f47b17adf17683aa9d2f4ee90bc6b23
amina963 <61815913+amina963@users.noreply.github.com>
2020-04-15 03:38:19 +0200

    Update SettingController.php

--- Laravel/app/Http/Controllers/SettingController.php
+++ Laravel/app/Http/Controllers/SettingController.php
@@ -2,107 +2,116 @@
 
 namespace App\Http\Controllers;
 
-use App\Certificate;
-use App\Course;
-use App\Education;
-use App\Experience;
-use App\Gender;
-use App\Image;
-use App\Language;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Intervention\Image\Facades\Image;
 use App\User;
+
+
+
+
 class SettingController extends Controller
 {
+    public function store(Request $request , $user) {
 
-    public function store(Request $request) {
+        $request->validate(['title' => 'bail|required|min:3', 'body' => 'required', 'photo' => 'image|mimes:jpeg,png,jpg|max:2048', 'first_name' => 'required', 'email'
+        =>'required', 'phone_number' => 'required' , 'address' =>'required']);
 
-        $request->validate([
+        //$post=new Image();
+        $user = Auth::User();
+        $user->first_name=$request->get('first_name');
+        $user->email=$request->get('email');
+        $user->phone_number=$request->get('phone_number');
+        //$user->age=$request->get('age');
+        $user->address=$request->get('address');
+        //$post=new Gender();
+        //$post->gendertype=$request->get('name');
 
-            'profileImage' =>'image|mimes:jpg,bmp,png|max:1999'
-        ]);
-        if ($request->hasFile('profileImage')) {
-            $file = $request->file('profileImage') ;
-            $ext = $file->getClientOriginalExtension() ;
-            $filename = 'profileImage' . '_' . time() . '.' . $ext ;
-            $path = $file->storeAs('public/Images', $filename);
+        //$post->user_id = $user->id;
+
+
+        //Upload image
+        if ($request->hasFile('name')) {
+            $name = $request->input(name);
+            $filename = time() .'-'. $name->getClientOriginalName();
+            $location = public_path('assets/images/'.$filename);
+
+            Image::make($name)->resize(800, 400)->save($location);
+
+            $user->name = $filename;
 
         } else {
-            $filename = 'noimage.jpg';
+            $name = 'noimage.jpg';
         }
 
 
-        $postimg  = new Image() ;
-        $postimg->images = $filename;
-        $postimg->save();
-        $postimg->name = image()->image()->id;
 
+        $user->save($request->all());
 
+       // return redirect('/settings(Employee)')->with('success','Post Created Successfully');
 
-        $post=new Image();
-        $post->path=$request->$path;
-        $post=new user();
-        $post->first_name=$request->get('first_name');
-        $post->email=$request->get('email');
-        $post->phone_number=$request->get('phone_number');
-        $post->age=$request->get('age');
-        $post->rate=$request->get('rating');
-        $post=new Gender();
-        $post->gendertype=$request->get('name');
+        return redirect('/userprofile');
 
-
-        $post->save();
-        return redirect('/companyprofile')->with('status','account saved');
-        //return redirect('/AccountSettings(Employee)')->with('status', 'Post was created !');
-        return view('user.settings(Employee)',['post'=>$post]);
+        //return view('user.settings(Employee)',['post'=>$post]);
     }
 
 
 
     public function received()
     {
-        $names = User::take(1)->get('username');
-        $emails = User::take(1)->get('email');
-        $phone = User::take(1)->get('phone_number');
-        $age=User::take(1)->get('age');
-        $workfield=Experience::take(1)->get('job_name');
-        $gendertype=Gender::take(1)->get('name');
-        $profession=Experience::take(1)->get('job_name');
-        $education=Education::take(1)->get('school');
-        $course=Course::take(1)->get('name');
-        $certificates=Certificate::take(1)->get('name');
-        $rate=User::take(1)->get('rating');
-        $totalproject=Experience::take(1)->get('total_project');
-        $englishlevel=Language::take(1)->get('english_level');
+        $user = Auth::User();
+        //$names = User::take(1)->get('username');
+       // $emails = User::take(1)->get('email');
+        //$phone = User::take(1)->get('phone_number');
+       // $age=User::take(1)->get('age');
+       // $workfield=Experience::take(1)->get('job_name');
+       // $gendertype=Gender::take(1)->get('name');
+        //$profession=Experience::take(1)->get('job_name');
+       // $education=Education::take(1)->get('school');
+        //$course=Course::take(1)->get('name');
+        //$certificates=Certificate::take(1)->get('name');
+        //$rate=User::take(1)->get('rating');
+        //$totalproject=Experience::take(1)->get('total_project');
+        //$englishlevel=Language::take(1)->get('english_level');
 
 
-        return view('user.userprofile', compact('emails', 'names', 'phone','age', 'workfield','gendertype','profession' ,'education','course','certificates','rate','totalproject','englishlevel'));
-
-
-    }
-    public function create()
-    {
         return view('user.userprofile');
+        //compact('emails', 'names', 'phone','rate'));
     }
 
-    public function edit($id){
-        $newdata=user::find($id);
-        return view('user.settings(Employee)',compact('newdata'));
+
+//    public function edit($id){
+//        $newdata = Auth::id();
+//        return view('user.settings(Employee)',compact('newdata'));
+//    }
+    public function getUpdate()
+    {
+        return view('user.settingsemployee');
     }
 
 
     public function update(Request$request,$id){
-        $request->validate(['email'=> 'required','phone_number'=> 'required','first_name'=> 'required']);
-        $newdata=user::find($id);
-        $newdata->id=$request->id;
-        $newdata->first_name=$request->first_name;
-        $newdata->email=$request->email;
-        $newdata->phone_number=$request->phone_number;
-        $newdata ->age= $request->age;
-        $newdata ->gender= $request->name;
-        $newdata->save();
-        return redirect('/settings(Employee)')->with('status','newdata updated');
+
+        $user_id= Auth::user()->id;
+        $user = Auth::User();
+        $user->first_name=$request->get('first_name');
+        $user->email=$request->get('email');
+        $user->phone_number=$request->get('phone_number');
+       // $user->age= $request->get('age');
+        $user->address= $request->get('address');
+        $user->save();
+
+        $result = Auth::attempt([
+            'username' => $request->get('username'),
+            'password' => $request->get('password'),
+            'email' => $request->get('email'),
+            'phone_number' => $request->get('phone_number'),
+            'address' => $request->get('address')
+        ]);
+        return redirect('/settingsemployee');
+
     }
-
-
 
 }
