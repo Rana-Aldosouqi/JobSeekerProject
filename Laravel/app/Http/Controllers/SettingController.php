2d89246130bdc3c4efb4a10d8936b1dd934634d0
amina963 <61815913+amina963@users.noreply.github.com>
2020-04-15 03:40:27 +0200

    Update SettingController.php

--- Laravel/app/Http/Controllers/SettingController.php
+++ Laravel/app/Http/Controllers/SettingController.php
@@ -19,17 +19,13 @@ class SettingController extends Controller
         $request->validate(['title' => 'bail|required|min:3', 'body' => 'required', 'photo' => 'image|mimes:jpeg,png,jpg|max:2048', 'first_name' => 'required', 'email'
         =>'required', 'phone_number' => 'required' , 'address' =>'required']);
 
-        //$post=new Image();
         $user = Auth::User();
         $user->first_name=$request->get('first_name');
         $user->email=$request->get('email');
         $user->phone_number=$request->get('phone_number');
         //$user->age=$request->get('age');
         $user->address=$request->get('address');
-        //$post=new Gender();
-        //$post->gendertype=$request->get('name');
-
-        //$post->user_id = $user->id;
+      
 
 
         //Upload image
@@ -50,11 +46,8 @@ class SettingController extends Controller
 
         $user->save($request->all());
 
-       // return redirect('/settings(Employee)')->with('success','Post Created Successfully');
-
+ 
         return redirect('/userprofile');
-
-        //return view('user.settings(Employee)',['post'=>$post]);
     }
 
 
@@ -62,30 +55,12 @@ class SettingController extends Controller
     public function received()
     {
         $user = Auth::User();
-        //$names = User::take(1)->get('username');
-       // $emails = User::take(1)->get('email');
-        //$phone = User::take(1)->get('phone_number');
-       // $age=User::take(1)->get('age');
-       // $workfield=Experience::take(1)->get('job_name');
-       // $gendertype=Gender::take(1)->get('name');
-        //$profession=Experience::take(1)->get('job_name');
-       // $education=Education::take(1)->get('school');
-        //$course=Course::take(1)->get('name');
-        //$certificates=Certificate::take(1)->get('name');
-        //$rate=User::take(1)->get('rating');
-        //$totalproject=Experience::take(1)->get('total_project');
-        //$englishlevel=Language::take(1)->get('english_level');
-
-
+    
         return view('user.userprofile');
-        //compact('emails', 'names', 'phone','rate'));
+        
     }
 
 
-//    public function edit($id){
-//        $newdata = Auth::id();
-//        return view('user.settings(Employee)',compact('newdata'));
-//    }
     public function getUpdate()
     {
         return view('user.settingsemployee');
